0340c1a17cf5e44c368f0c010e18abcb4e23c380
Joseph Caburnay <caburnay.joseph@gmail.com>
2024-01-22 21:19:00 +0100

    imp: shorter tick name of colorbar coulomb

--- PyVQ/pyvq/pyvq.py
+++ PyVQ/pyvq/pyvq.py
@@ -1691,8 +1691,6 @@ class FieldPlotter:
             self.dmc['cmap'] = plt.get_cmap('seismic')
             self.dmc['cbar_min'] = -cbar_max
             self.dmc['cbar_max'] = cbar_max
-            self.dmc['cbar_min_coulomb'] = -cbar_max * 1.10
-            self.dmc['cbar_max_coulomb'] = cbar_max * 1.10
             if self.field_type == 'gravity' or self.field_type == 'dilat_gravity' or self.field_type == 'satellite_gravity':
                 self.dmc['cb_fontsize'] = 20.0
             elif self.field_type == 'potential':
@@ -2246,16 +2244,31 @@ class FieldPlotter:
         cb_ax = fig4.add_axes((left_frac,bottom_frac,width_frac,height_frac))
         norm = self.norm
 
-        if self.field_type == 'coulomb':
-            self.norm = mcolor.Normalize(vmin=self.dmc['cbar_min_coulomb'], vmax=self.dmc['cbar_max_coulomb'])
-
         cb = mcolorbar.ColorbarBase(cb_ax, cmap=cmap,
                norm=norm,
                orientation='horizontal')
 
         if self.field_type == 'coulomb':
-            coulomb_ticks = [self.dmc['cbar_min'], self.dmc['cbar_min'] / 2, 0, self.dmc['cbar_max'] / 2, self.dmc['cbar_max']]
-            coulomb_tick_labels = ['{:.0e}'.format(x) for x in coulomb_ticks]
+
+            def fmt(v):
+                if abs(v) < 0.001:
+                    return '0'
+
+                sign = -1 if v < 0 else 1
+                v = abs(v)
+                exp = math.floor(math.log10(v))
+                base = v / 10 ** exp
+                res = ''
+                if exp == 0:
+                    res = '{:.0f}'.format(base)
+                else:
+                    res = '{:.0f}e{}'.format(base, int(exp))
+                if sign == -1:
+                    res = '-{}'.format(res)
+                return res
+
+            coulomb_ticks = np.linspace(self.dmc['cbar_min'], self.dmc['cbar_max'], num=9)
+            coulomb_tick_labels = [fmt(x) for x in coulomb_ticks]
             cb.set_ticks(coulomb_ticks)
             cb.set_ticklabels(coulomb_tick_labels)
 
