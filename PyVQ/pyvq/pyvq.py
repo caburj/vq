57f95b5c86e95708d56e8d9a871d7eb3a0f93a7f
Joseph Caburnay <caburnay.joseph@gmail.com>
2024-04-14 00:01:12 +0200

    move c inside open block

--- PyVQ/pyvq/pyvq.py
+++ PyVQ/pyvq/pyvq.py
@@ -3472,12 +3472,12 @@ if __name__ == "__main__":
                              
 
     if args.generate_vertex_4:
-        base = geometry.model.get_base()
-        base_lld = quakelib.LatLonDepth(base[0], base[1], 0.0)
-        c = quakelib.Conversion(base_lld)
-
         filename = "vertex_4.csv"
         with open(filename, "w") as f:
+
+            base = geometry.model.get_base()
+            base_lld = quakelib.LatLonDepth(base[0], base[1], 0.0)
+            c = quakelib.Conversion(base_lld)
 
             columns = ["element_id", "lat", "lon", "alt"]
             f.write(",".join(columns)+"\n")
